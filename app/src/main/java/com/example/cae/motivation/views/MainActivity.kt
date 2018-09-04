1aefb471a3c0e8d5b99cfabf82a1995eda533833
Caê Gonçalves <cae.flops@gmail.com>
2018-09-04 17:40:24 -0300

    Some modifications on Activities package and manifest, adding a new style to the main activity

--- app/src/main/java/com/example/cae/motivation/views/MainActivity.kt
+++ app/src/main/java/com/example/cae/motivation/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.cae.motivation
+package com.example.cae.motivation.views
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cae.motivation.R
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/cae/motivation/views/SplashActivity.kt
+++ app/src/main/java/com/example/cae/motivation/views/SplashActivity.kt
@@ -1,8 +1,11 @@
-package com.example.cae.motivation
+package com.example.cae.motivation.views
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.example.cae.motivation.R
 import com.example.cae.motivation.util.AppConstants
 import com.example.cae.motivation.util.SecurityPreferences
 import kotlinx.android.synthetic.main.activity_splash.*
@@ -23,7 +26,7 @@ class SplashActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mSecurity : SecurityPreferences
 
     override fun onClick(view: View?) {
-        val id = view.id
+        val id = view?.id
         if (id == R.id.buttonSave) {
             handleSave()
         }
@@ -31,7 +34,15 @@ class SplashActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun handleSave() {
         val name: String = editName.text.toString()
-        mSecurity.storeString(AppConstants.KEY.PERSON_NAME, name)
+
+        if (name == "") {
+            Toast.makeText(this, getString(R.string.notice_no_name), Toast.LENGTH_LONG).show()
+        } else {
+            mSecurity.storeString(AppConstants.KEY.PERSON_NAME, name)
+
+            val intent: Intent = Intent(this, MainActivity::class.java) //a classe que temos a intenção de abrir, por isso o uso de ::, pois não é uma instância, mas sim uma referência
+            startActivity(intent)
+        }
 
     }
 
